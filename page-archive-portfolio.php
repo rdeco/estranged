bc39dffb26c0e08949ad68c593f79371cf7f9ac0
rdeco <rdecodesigns@gmail.com>
2015-06-09 20:08:27 -1000

    changed loop & contents to coincide with CPT UI & Advanced Custom Fields plug-ins

--- page-archive-portfolio.php
+++ page-archive-portfolio.php
@@ -2,7 +2,7 @@
 	/*
 		Template Name: Portfolio
 	*/
-
+	
 	get_header(); ?>
     <div class="container portfolioSection">
         <div class="row">
@@ -10,26 +10,19 @@
 	        	<h1>Portfolio</h1>
 	        </div>
         </div>
-        <div class="col-xs-12 portfolioPage">            
-            <?php
+        <div class="col-xs-12 portfolioPage">  
+	        <?php	            
                 $wp_query = new WP_Query();
                 $wp_query->query('posts_per_page=6&post_type=portfolio'.'&paged='.$paged);
 
                 while ($wp_query->have_posts()) {
                     $wp_query->the_post();
-            ?>
+            ?>           
             <div class="view view-fifth">
-                <?php
-                    if ( has_post_thumbnail() ) {
-                        echo the_post_thumbnail('archive-portfolio-thumb');
-                ?>
-                <?php }
-                else {  ?>
-                    <img src="<?php print IMAGES; ?>/alt-img_350x500.jpg" alt="img02">
-                <?php } ?>
+                <?php the_post_thumbnail('large'); ?>
                 <div class="mask">
                     <h2><?php the_title(); ?></h2>
-                    <p><?php the_excerpt(); ?> </p>
+                    <?php the_excerpt(); ?> 
                     <a href="<?php the_permalink(); ?>" class="info">See Details</a>
                 </div>
             </div> <!-- .view .view-fifth -->
@@ -46,4 +39,3 @@
         </nav>
     <?php endif; ?>
 <?php get_footer(); ?>
-
