406a7b90be4d10d210ec0bc8deae6e1ef25a1307
rdeco <rdecodesigns@gmail.com>
2015-06-10 12:00:55 -1000

    changed <div> to <article>

--- page.php
+++ page.php
@@ -1,33 +1,27 @@
-<?php
-/*
-* Template Name: General Page
-*/
-?>
-
-<?php get_header(); ?>
-        <div class="container aboutPage">
-	        <div class="row">
-		        <div class="col-xs-12 archiveTitle">
-		        	<h1><?php wp_title(' '); ?></h1>
-		        </div>
-	        </div>
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1 col-xs-12 aboutPage">       
-
-                    <?php
-                    if (have_posts()) {
-                        while(have_posts()) {
-                            the_post();
-                    ?>
-                    <p class="aboutText">
-                       <?php the_content(); ?>
-                    </p>
-
-                     <?php
-                         }
-                     }
-                     ?>
-                </div> <!-- .col-sm-10 .col-sm-offset-1 .col-xs-12 .aboutPage -->
-            </div><!-- .row -->
-        </div> <!-- .container .aboutPage -->
+<?php get_header(); ?>			  	
+	<!-- start of front page content-->			  	
+	<section class="page-img col-xs-12">     		
+		<div class="row">
+			<div class="page col-md-6 col-md-offset-6">
+				<?php 
+					if(have_posts()) {
+						while(have_posts()) {
+							the_post();
+				?>
+				<div class="row">
+					<h1 class="page-h1">
+						<?php the_title(); ?>
+					</h1>
+					<div class="page-para">
+						<?php the_content(); ?>	 	
+					</div>
+				</div>
+				<?php
+						}	
+					}	
+				?>
+				
+			</div>
+		</div><!-- .row -->
+	</section><!-- .col-xs-12 -->
 <?php get_footer(); ?>
