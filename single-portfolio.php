ad8482b97f5f557a5cb498036ceebeae9ea82c22
rdeco <rdecodesigns@gmail.com>
2015-06-10 13:09:33 -1000

    changed class names

--- single-portfolio.php
+++ single-portfolio.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-    <section class="container">
+    <div class="container">
     	<div class="row">
             <div class="col-xs-12  productPage">
 	            <?php
@@ -8,17 +8,18 @@
 							the_post();				
 	            ?>
 	            <div class="row">
-                    <article class="col-md-6 col-xs-12 single-portfolio">
+                    <section class="col-md-6 col-xs-12 single-portfolio">
                         <h1 class="productPageTitle" ><?php the_title(); ?></h1>
                         <div class="productInfo">
                             <ul class="productInfoUl">
-                                <li class="productInfo1 website"> <span class="website">website:</span>                                  
-									<?php the_field( 'website' ); ?>                                   
-                                </li>
-                                <li class="productInfo2 company"><span class="company">company:</span>
+	                            
+	                            <li class="product-info-company"><span class="company">company:</span>
 									<?php the_field('company_name'); ?>
                                 </li>
-                                <li class="productInfo3"><span class="date">date:</span>
+                                <li class="product-info-website"> <span class="website">website:</span>                                  
+									<?php the_field( 'website' ); ?>                                   
+                                </li>
+                                <li class="product-info-date"><span class="date">date:</span>
                                     <div class="date">
 										<?php the_field( 'date' ); ?>
                                     </div>
@@ -26,12 +27,12 @@
                             </ul>
                         </div>
                         <hr class="style single-portfolio">
-                        <p class="productText"><?php the_content(); ?>  </p>
+                        <?php the_content(); ?>
                         <!-- media share buttons, Go to www.addthis.com/dashboard to customize tools  -->
                         <div class="addthis_sharing_toolbox"></div>
                         <!-- end of media share buttons -->
-                    </article>    
-                    <div class="col-md-6 col-xs-12 single-portfolio">
+                    </section>    
+                    <article class="col-md-6 col-xs-12 single-portfolio">
 	                    <div class="row">
 			                <div class="col-md-6 col-xs-12 single-portfolio">
 		                        <figure class="fig1">
@@ -39,7 +40,7 @@
 		                        </figure>
 			                </div>
 	                    </div>
-                    </div>
+                    </article>
                 </div> <!-- .row -->
                 <?php
 	                 } 
@@ -47,7 +48,7 @@
 	                ?>
             </div> <!-- .col-xs-12 .productPage -->
         </div> <!-- .row -->            
-    </section> <!-- .container -->
+    </div> <!-- .container -->
     <nav class="archive-portfolio pagination">
         <?php previous_post_link(); ?>  
         	<a href="<?php bloginfo('url'); ?>/?p=51"><i class="fa fa-th-large"></i></a>
