68fd1115c5452ca006a075f184b852606ec440eb
rdeco <rdecodesigns@gmail.com>
2015-06-09 15:52:37 -1000

    fixed pagination

---
+++ page-archive-portfolio.php
@@ -0,0 +1,49 @@
+<?php
+	/*
+		Template Name: Portfolio
+	*/
+
+	get_header(); ?>
+    <div class="container portfolioSection">
+        <div class="row">
+	        <div class="col-xs-12 archiveTitle">	        
+	        	<h1>Portfolio</h1>
+	        </div>
+        </div>
+        <div class="col-xs-12 portfolioPage">            
+            <?php
+                $wp_query = new WP_Query();
+                $wp_query->query('posts_per_page=6&post_type=portfolio'.'&paged='.$paged);
+
+                while ($wp_query->have_posts()) {
+                    $wp_query->the_post();
+            ?>
+            <div class="view view-fifth">
+                <?php
+                    if ( has_post_thumbnail() ) {
+                        echo the_post_thumbnail('archive-portfolio-thumb');
+                ?>
+                <?php }
+                else {  ?>
+                    <img src="<?php print IMAGES; ?>/alt-img_350x500.jpg" alt="img02">
+                <?php } ?>
+                <div class="mask">
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php the_excerpt(); ?> </p>
+                    <a href="<?php the_permalink(); ?>" class="info">See Details</a>
+                </div>
+            </div> <!-- .view .view-fifth -->
+            <?php
+                }
+            ?>
+        </div> <!-- .col-xs-12 .portfolioPage -->
+    </div> <!-- .container .portfolioSection -->
+    <?php if (show_posts_nav()) : ?>
+        <nav class="archive-portfolio pagination">
+                <?php previous_posts_link('&laquo; Previous Page'); ?>  
+                	<a href="<?php bloginfo('url'); ?>/?p=51"><i class="fa fa-th-large"></i></a>
+                <?php next_posts_link('Next Page  &raquo;'); ?>
+        </nav>
+    <?php endif; ?>
+<?php get_footer(); ?>
+
