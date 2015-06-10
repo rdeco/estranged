6175ad011efd0ddb220f19b130f95355531485cb
rdeco <rdecodesigns@gmail.com>
2015-06-10 11:35:29 -1000

    fixed loop • added author name • changed pagination • changed <div> to <section>

--- home.php
+++ home.php
@@ -1,11 +1,5 @@
-<?php
-/*
-* Template Name: Blog Archives
-*/
-?>
-
 <?php get_header(); ?>
-    <div class="container blogArchive">
+    <section class="container blogArchive">
 	    <div class="row">
             <div class="col-lrg-2 col-lrg-offset-10 col-xs-12 blogArchivePage">
                 <aside>
@@ -15,16 +9,14 @@
         </div>
         <div class="row">       
 	        <div class="col-xs-12 blogArchive">
-	            <div class="blogArchive">
-		            
+	            <div class="blogArchive">	            
 		            
 	                <?php
-	
-	                    $wp_query = new WP_Query();
-	                    $wp_query->query('showposts=5'.'&paged='.$paged);
-	
-	                    while ($wp_query->have_posts()) : $wp_query->the_post();
-	                 ?>
+						
+					if(have_posts()) {
+						while(have_posts()){
+							the_post();
+	                ?>
 	
 	                <div class="col-sm-2 blogDate">
 	                    <p class="month"><?php the_time('M'); ?></p>
@@ -34,6 +26,7 @@
 	                
 	                <article class="col-sm-6 blogArticle">
 	                	<h1 class="blogTitle"><?php the_title(); ?></h1>
+	                	<p>Written By: <span class="blog-author-name"><?php the_author(); ?></span></p>
 	                    <a class="blogArchive" href="<?php the_permalink(); ?>">
 	                        <p class="blogText"><?php the_excerpt(); ?></p>
 	                    </a>
@@ -59,19 +52,19 @@
                         <?php } ?>	                  
 	                </figure>
 	                <?php
-	                   endwhile; wp_reset_postdata();
+	                	}
+					}	
 	                ?>
 	            </div> <!-- .blogArchive -->
 	        </div> <!-- .col-xs-12 .blogArchive -->
-    	</div> <!-- .row -->
-    
-	    <?php if (show_posts_nav()) : ?>
-	        <nav  class="nav-blog-archive">
-	            <div class="pager blogArchive">
-	                <?php previous_posts_link('&laquo; Previous Posts') ?>||<?php next_posts_link('More Posts &raquo;') ?>
-	            </div>
-	        </nav>
+    	</div> <!-- .row -->    
+	    <?php if (show_posts_nav()) : ?>	   
+	        <div class="pager blogArchive">
+	            <?php previous_posts_link('&laquo; Previous Posts') ?>
+	            	<a href="<?php bloginfo('url'); ?>/?p=6"><i class="fa fa-th-large"></i></a>
+	            <?php next_posts_link('More Posts &raquo;') ?>
+	        </div>	  
 		<?php endif; ?>
-	</div>
+	</section> <!-- .container .blogArchive -->
 <?php get_footer(); ?>
 
