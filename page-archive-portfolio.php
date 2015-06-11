317afe8f8d9f0fcb9a7d74e11180114dc49efd41
rdeco <rdecodesigns@gmail.com>
2015-06-10 16:23:59 -1000

    changed pagination class

--- page-archive-portfolio.php
+++ page-archive-portfolio.php
@@ -5,16 +5,16 @@
 	
 	get_header(); ?>
     <section class="container portfolioSection">
-        <div class="row">
-	        <h1 class="archiveTitle">Portfolio</h1>   
-	        <div class="col-xs-12 portfolioPage">  
+        <div class="row"> 
+	        <h1 class="archiveTitle"><?php the_title(); ?></h1>    
+	        <div class="col-xs-12 portfolioPage">       
 		        <?php	            
 	                $wp_query = new WP_Query();
 	                $wp_query->query('posts_per_page=6&post_type=portfolio'.'&paged='.$paged);
 	
 	                while ($wp_query->have_posts()) {
 	                    $wp_query->the_post();
-	            ?>           
+	            ?>        
 	            <div class="view view-fifth">
 	                <?php the_post_thumbnail('large'); ?>
 	                <div class="mask">
@@ -30,7 +30,7 @@
 	    </div>
 	</section> <!-- .container .portfolioSection -->
     <?php if (show_posts_nav()) : ?>
-        <nav class="archive-portfolio pagination">
+        <nav class="pagination">
 	        <?php previous_posts_link('&laquo; Previous Page'); ?>  
 	        	<a href="<?php bloginfo('url'); ?>/?p=51"><i class="fa fa-th-large"></i></a>
 	        <?php next_posts_link('Next Page  &raquo;'); ?>
